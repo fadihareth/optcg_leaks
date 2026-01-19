9970d99e17f4e396df3c60182742aded42c32fa3
Fadi Hareth <fadihareth@gmail.com>
2026-01-19 18:33:28 -0500

    remove some unnecessary styling

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -9,15 +9,15 @@
 </script>
 
 <header
-	class="p-layout sticky top-0 z-50 flex items-center justify-between bg-theme/80 shadow-sm backdrop-blur"
+	class="p-layout sticky top-0 z-50 flex items-center justify-between bg-theme shadow-xs"
 >
-	<h1 class="text-xl">OPTCG Leaks</h1>
-	<Select
+	<h1 class="text-xl font-semibold">OPTCG Leaks</h1>
+	<!-- <Select
 		items={sets}
 		value={selectedSet}
 		bind:justValue={selectedSet}
 		clearable={false}
-		class={`hidden! w-20! bg-theme! ${loading && 'opacity-0'}`}
+		class={`w-20! bg-theme! ${loading && 'opacity-0'}`}
 		placeholder=""
-	/>
+	/> -->
 </header>
