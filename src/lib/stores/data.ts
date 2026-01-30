4279a2212791533a4b043957735fbedada1598a8
Fadi Hareth <fadihareth@gmail.com>
2026-01-30 10:53:14 -0500

    remove fetchSets call

--- src/lib/stores/data.ts
+++ src/lib/stores/data.ts
@@ -5,11 +5,6 @@ import { CardSet } from "$lib/models/CardSet";
 import { fetchJson } from "$lib/util";
 import { baseURL } from "$lib/constants";
 
-export async function fetchSets() {
-    const sets: { "availableSets": string[] } = await fetchJson(`${baseURL}/sets.json`);
-    return sets.availableSets;
-}
-
 export async function loadData(set: string) {
     const fetchedData: { "data": CardSetJSON, "cards": CardJSON[] } = await fetchJson(`${baseURL}/${set}/${set}.json`);
     return {
