78d3f31192df5ed91a9b1f94781922c7bce175e0
Fadi Hareth <fadihareth@gmail.com>
2026-08-19 14:43:21 -0400

    update set_ids

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const baseURL: string = "https://images.optcgleaks.com";
 
-export const SET_IDS = ['op17', 'st31', 'st32', 'st33', 'st34', 'st35', 'st36'] as const;
+export const SET_IDS = ['op17', 'op18'] as const;
